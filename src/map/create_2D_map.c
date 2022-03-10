369c52d0e2c3858360d9404bdb35ca94b0c9dcda
iMeaNz <mehdy.morvan@epitech.eu>
2022-03-10 13:02:59 +0100

    Now calculate distance to find the exact center of the map and then translate

--- src/map/create_2D_map.c
+++ src/map/create_2D_map.c
@@ -22,29 +22,32 @@ sfVector2f project_iso_point(float x, float y, float z, sfVector2f factors)
     return vector;
 }
 
-//TODO: prendre milieu de la distance
 void translate_map(sfVector2f **map_2d, sfVector3f **map_3d,
                     sfVector2f factors)
 {
     sfVector2f center_tile;
-    sfVector2f fist_x_tile;
-    sfVector2f last_x_tile;
-    sfVector2f last_y_tile;
     float rad_x = DEG_TO_RAD(35);
     float rad_y = DEG_TO_RAD(25);
-    float c_x = map_3d[MAP_X / 2][MAP_X / 2].x;
-    float c_y = map_3d[MAP_Y / 2][MAP_Y / 2].y;
-
-    center_tile.x = (cos(rad_x) * c_x - (cos(rad_x) * c_y)) * factors.x;
-    center_tile.y = (sin(rad_y) * c_y + sin(rad_y) * c_x) * factors.y;
-    int offset_x = 930 - center_tile.x;
-    int offset_y = 510 - center_tile.y;
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    float c_x = (map_3d[0][0].x + map_3d[MAP_Y - 1][MAP_X - 1].x) / 2;
+    float c_y = (map_3d[MAP_X - 1][0].y + map_3d[0][MAP_Y - 1].y) / 2;
+    sfVector2f center_point = project_iso_point(c_x, c_y, 0, factors);
+    int offset_x = 930 - center_point.x;
+    int offset_y = 510 - center_point.y;
+    for (int i = 0; i < MAP_Y; i++) {
+        for (int j = 0; j < MAP_X; j++) {
             map_2d[i][j].x += offset_x;
             map_2d[i][j].y += offset_y;
         }
     }
+}
+
+sfVector2f project_3D_to_2D(sfVector3f coords_3D, sfVector2f factors)
+{
+    float x  = coords_3D.x;
+    float y = coords_3D.y;
+    float z = coords_3D.z;
+    sfVector2f coords_2D = project_iso_point(x, y, z, factors);
+    return coords_2D;
 }
 
 sfVector2f **create_2d_map(sfVector3f **map_3d, data_t data)
@@ -52,12 +55,8 @@ sfVector2f **create_2d_map(sfVector3f **map_3d, data_t data)
     sfVector2f **output = malloc(sizeof(sfVector2f *) * MAP_Y);
     for (int i = 0; i < MAP_Y; i++) {
         output[i] = malloc(sizeof(sfVector2f) * MAP_X);
-        for (int j = 0; j < MAP_X; j++) {
-            float x = map_3d[i][j].x;
-            float y = map_3d[i][j].y;
-            float z = map_3d[i][j].z;
-            output[i][j] = project_iso_point(x, y, z, data.map.factors);
-        }
+        for (int j = 0; j < MAP_X; j++)
+            output[i][j] = project_3D_to_2D(map_3d[i][j], data.map.factors);
     }
     translate_map(output, map_3d, data.map.factors);
     return output;
