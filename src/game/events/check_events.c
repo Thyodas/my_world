e639cd46edca3d89deca5f7691a3f9cae9aa9238
iMeaNz <mehdy.morvan@epitech.eu>
2022-03-10 10:36:17 +0100

    Removed useless commentary, data is now passed as a pointer in order to change stuff

--- src/game/events/check_events.c
+++ src/game/events/check_events.c
@@ -10,38 +10,24 @@
 #include <math.h>
 #include <stdlib.h>
 
-void check_event(data_t data)
+void check_event(data_t *data)
 {
     float angle = 1 * M_PI / 180;
-    sfVector3f **map = data.map.array_3d;
-    while (sfRenderWindow_pollEvent(data.window, &data.event) == sfTrue) {
-        if (data.event.type == sfEvtClosed)
-            sfRenderWindow_close(data.window);
-        if (data.event.type == sfEvtKeyPressed && data.event.key.code ==
+    sfVector3f **map = data->map.array_3d;
+    while (sfRenderWindow_pollEvent(data->window, &data->event) == sfTrue) {
+        if (data->event.type == sfEvtClosed)
+            sfRenderWindow_close(data->window);
+        if (data->event.type == sfEvtKeyPressed && data->event.key.code ==
         sfKeyDown) {
-            //for (int i = 0; i < 10; i++) {
-            //    for (int j = 0; j  < 10; j++) {
-            //        map[i][j].x = (1 - (1 - cos(angle)) / 2) * map[i][j].x - ((1 - cos(angle)) / 2) * map[i][j].y - ((sin(angle)) / sqrt(2)) * map[i][j].z;
-            //        map[i][j].y = (1 - (1 - cos(angle)) / 2) * map[i][j].y - ((1 - cos(angle)) / 2) * map[i][j].x - ((sin(angle)) / sqrt(2)) * map[i][j].z;
-            //        map[i][j].z = (sin(angle) / sqrt(2)) * map[i][j].x + (sin(angle) / sqrt(2)) * map[i][j].y + cos(angle) * map[i][j].z;
-            //    }
-            //}
-            data.map.factors.x -= 5;
-            data.map.factors.y -= 5;
+            data->map.factors.x -= 5;
+            data->map.factors.y -= 5;
         }
-        if (data.event.type == sfEvtKeyPressed && data.event.key.code ==
+        if (data->event.type == sfEvtKeyPressed && data->event.key.code ==
         sfKeyUp) {
-            //for (int i = 0; i < 10; i++) {
-            //    for (int j = 0; j  < 10; j++) {
-            //        map[i][j].x = (1 - (1 - cos(-angle)) / 2) * map[i][j].x - ((1 - cos(-angle)) / 2) * map[i][j].y - ((sin(-angle)) / sqrt(2)) * map[i][j].z;
-            //        map[i][j].y = (1 - (1 - cos(-angle)) / 2) * map[i][j].y - ((1 - cos(-angle)) / 2) * map[i][j].x - ((sin(-angle)) / sqrt(2)) * map[i][j].z;
-            //        map[i][j].z = (sin(-angle) / sqrt(2)) * map[i][j].x + (sin(-angle) / sqrt(2)) * map[i][j].y + cos(-angle) * map[i][j].z;
-            //    }
-            //}
-            data.map.factors.x += 5;
-            data.map.factors.y += 5;
+            data->map.factors.x += 5;
+            data->map.factors.y += 5;
         }
-        if (data.event.type == sfEvtKeyPressed && data.event.key.code ==
+        if (data->event.type == sfEvtKeyPressed && data->event.key.code ==
         sfKeyRight) {
             for (int i = 0; i < MAP_Y; i++) {
                 for (int j = 0; j < MAP_X; j++) {
@@ -50,7 +36,7 @@ void check_event(data_t data)
                 }
             }
         }
-        if (data.event.type == sfEvtKeyPressed && data.event.key.code ==
+        if (data->event.type == sfEvtKeyPressed && data->event.key.code ==
         sfKeyLeft) {
             for (int i = 0; i < MAP_Y; i++) {
                 for (int j = 0; j < MAP_X; j++) {
