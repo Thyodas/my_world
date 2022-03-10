a947fd40ec8af759469ddc3c2716a6e7592cefb9
iMeaNz <mehdy.morvan@epitech.eu>
2022-03-10 20:58:08 +0100

    PERF: Now free the 3d map at the end of the program

--- src/main.c
+++ src/main.c
@@ -9,11 +9,13 @@
 
 void main_loop(data_t data);
 sfVector3f **init_map_3d(int map_x, int map_y);
+void free_3d_map(sfVector3f **map_3d);
 
 int main(int argc, char **argv)
 {
     data_t data;
     data.map.array_3d = init_map_3d(MAP_X, MAP_Y);
     main_loop(data);
+    free_3d_map(data.map.array_3d);
     return 0;
 }
--- src/map/utils_map/free_map.c
+++ src/map/utils_map/free_map.c
@@ -14,3 +14,10 @@ void free_2d_map(sfVector2f **map_2d)
         free(map_2d[i]);
     free(map_2d);
 }
+
+void free_3d_map(sfVector3f **map_3d)
+{
+    for (int i = 0; i < MAP_Y; ++i)
+        free(map_3d[i]);
+    free(map_3d);
+}
