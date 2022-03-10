e8b1155c0285a884f1047d639be9f14aab631393
iMeaNz <mehdy.morvan@epitech.eu>
2022-03-10 10:38:13 +0100

    Added function to initalize data struct

--- src/display/main_loop.c
+++ src/display/main_loop.c
@@ -11,25 +11,27 @@
 #include <stdlib.h>
 
 sfVector2f **create_2d_map(sfVector3f **map_3d, data_t data);
-void check_event(data_t data);
+void check_event(data_t *data);
 int draw_2d_map(data_t data);
+sfRenderWindow *init_window(int x, int y);
+void load_sand_texture(data_t *data);
+void load_checker_texture(data_t *data);
+
+void init_data(data_t *data)
+{
+    data->window = init_window(1920, 1080);
+    load_sand_texture(data);
+    load_checker_texture(data);
+    data->map.factors = (sfVector2f){100, 75};
+}
 
 void main_loop(data_t data)
 {
-    //map[0][1].z = 2;
-    sfVideoMode mode = {1920, 1080, 0};
-    data.window = sfRenderWindow_create(mode, "my_world", sfClose, NULL);
-    data.map.factors = (sfVector2f){100, 75};
-    sfRenderWindow_setFramerateLimit(data.window, 500);
-    data.textures.sand.texture = sfTexture_createFromFile("img/sand.jpg", NULL);
-    data.textures.sand.size = (sfVector2u){800, 709};
-    data.textures.checker.texture = sfTexture_createFromFile("img/check.jpg",
-                                                          NULL);
-    data.textures.checker.size = (sfVector2u){2048, 2048};
+    init_data(&data);
     while (sfRenderWindow_isOpen(data.window)) {
         data.map.array_2d = create_2d_map(data.map.array_3d, data);
         sfRenderWindow_clear(data.window, sfBlue);
-        check_event(data);
+        check_event(&data);
         draw_2d_map(data);
         sfRenderWindow_display(data.window);
     }
