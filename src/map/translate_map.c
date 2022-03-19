994254a48727c21e6b9f7efd25447e317e0ec54c
Thyodas <guillaume.hein@epitech.eu>
2022-03-19 16:53:39 +0100

    DEL: removed useless map files, ADD: variable size of map

--- src/map/translate_map.c
+++ src/map/translate_map.c
@@ -9,8 +9,8 @@
 
 void translate_map(data_t *data, sfVector2f vector)
 {
-    for (int i = 0; i < MAP_Y; ++i) {
-        for (int j = 0; j < MAP_X; ++j) {
+    for (int i = 0; i < data->map.size; ++i) {
+        for (int j = 0; j < data->map.size; ++j) {
             data->map.tiles[i][j].coord_3d.x += vector.x;
             data->map.tiles[i][j].coord_3d.y += vector.y;
         }
