96097037c78107567c47b4ef6dc26552cd1136bc
iMeaNz <mehdy.morvan@epitech.eu>
2022-03-20 19:12:43 +0100

    Added missing textures

--- src/game/save_map/load.c
+++ src/game/save_map/load.c
@@ -21,6 +21,10 @@ void implement_texture(data_t *data, int id, int i, int j)
         data->map.tiles[i][j].texture = data->textures.sand;
     if (id == GRASS)
         data->map.tiles[i][j].texture = data->textures.grass;
+    if (id == DIRT)
+        data->map.tiles[i][j].texture = data->textures.dirt;
+    if (id == STONE)
+        data->map.tiles[i][j].texture = data->textures.stone;
 }
 
 int fill_tile(char *line, int i, int j, data_t *data)
